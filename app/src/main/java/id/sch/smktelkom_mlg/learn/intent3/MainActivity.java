3aa3269f9c2e6cd6f22dbdab63558026b350f1ea
sasnahapsari <sasnahapsari@gmail.com>
2017-01-29 22:59:18 +0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,26 +1,30 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TELEPON
+
         findViewById(R.id.imageViewPhone).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialPhoneNumber("085733573529");
+                dialPhoneNumber("08598799845");
             }
         });
 
-        //SMS
         findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,23 +32,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //BROWSER
         findViewById(R.id.imageViewBrowser).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openWebPage("http://www.smktelkom-mlg.sch.id");
             }
         });
+
+        findViewById(R.id.imageViewCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capturePhoto();
+            }
+        });
     }
 
-    //Fungsi TELEPON
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 
-    //FUNGSI SMS
     public void composeSmsMessage(String message) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
@@ -52,10 +60,25 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }
 
-    //FUNGSI BROWSER
     public void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
+    }
+
+    public void capturePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
     }
 }
